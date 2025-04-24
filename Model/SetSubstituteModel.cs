4866324568c433e8b15ccff7cd3b3cb6ab664944
Paolo Escotido <escotidop@Ismanila.org>
2025-04-25 07:34:15 +0800

    Added List of request based on teachers id

---
+++ Model/SetSubstituteModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoverMate.Model
+{
+
+
+    //[AutoValidateAntiforgeryToken]
+    public class SetSubstituteModel
+    {
+        [Required(ErrorMessage = "request_id is required.")]
+        public string request_id { get; set; }
+
+
+        [Required(ErrorMessage = "substitute_id is required.")]
+        public string substitute_id { get; set; }
+
+
+        [Required(ErrorMessage = "assignee_id is required.")]
+        public string assignee_id { get; set; }
+
+
+    }
+}
